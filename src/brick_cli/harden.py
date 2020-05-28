f190e023a056d91567511a1940197bda683ee8b4
cray75 <craysmith75@gmail.com>
2020-05-28 13:29:20 -0500

    added hardening script

---
+++ src/brick_cli/harden.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+
+packages = ["fail2ban", "clamp", "clamav-daemon", "debsums", "aide", "libpam-cracklib",
+                "acct", "sysstat", "auditd", "gcc", "libpcre3-dev", "zliblg-dev", "libluajit-5.1-dev",
+                "libpcap-dev", "openssl", "libssl-dev", "libnghttp2-dev", "libdumbnet-dev", "bison", 
+                "flex", "libnetd", " zlib1g-dev "]
+
+def install_deps():
+    for package in packages:
+            process = subprocess.Popen('sudo apt-get install -y ' + package, shell=True, stdin=None, executable="/bin/bash")
+            process.wait()
+
+if __name__=="__main__":
+    install_deps()
