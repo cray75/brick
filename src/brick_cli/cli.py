8f6d5a631311f7576f1f4dd23f387a27e137630d
cray75 <craysmith75@gmail.com>
2020-05-28 12:51:36 -0500

    have click & basic UI formatting working

---
+++ src/brick_cli/cli.py
@@ -0,0 +1,35 @@
+import click
+import six
+from pyfiglet import figlet_format
+
+from . import __version__
+
+try:
+    import colorama
+    colorama.init()
+except ImportError:
+    colorama = None
+
+try:
+    from termcolor import colored
+except ImportError:
+    colored = None
+
+
+def log(string, color, font="block", figlet=False):
+    if colored:
+        if not figlet:
+            six.print_(colored(string, color))
+        else:
+            six.print_(colored(figlet_format(
+                string, font=font), color))
+    else:
+        six.print_(string)
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The Brick CLI Python project."""
+    log("Brick CLI", color="red", figlet=True)
+    log("Welcome to Brick CLI", "green")
+    click.echo("Hello, world!")
