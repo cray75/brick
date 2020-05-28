4fc9ec54b20b10ae708c1a12eb211b9881ce0dbc
cray75 <craysmith75@gmail.com>
2020-05-28 18:52:19 -0500

    refactor directory in prometheus

--- src/brick_cli/prometheus.py
+++ src/brick_cli/prometheus.py
@@ -3,17 +3,17 @@ import os
 
 def configure_node_exporter(): 
     server = "172.31.18.149"
-    os.chdir("~/")
+    os.chdir("/home/ubuntu")
     make_node_user = subprocess.Popen('useradd --no-create-home --shell /bin/false node_exporter', shell=True, stdin=None, executable="/bin/bash")
     make_node_user.wait()
     get_node_exporter = subprocess.Popen('wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz && tar xvf node_exporter-0.18.1.linux-amd64.tar.gz && cd node_exporter-0.18.1.linux-amd64',
                                           shell=True, stdin=None, executable="/bin/bash")
     get_node_exporter.wait()
-    os.chdir("~/node_exporter-0.18.1.linux-amd64")
+    os.chdir("/home/ubuntu/node_exporter-0.18.1.linux-amd64")
     set_permissions = subprocess.Popen('cp node_exporter /usr/local/bin && chown node_exporter:node_exporter /usr/local/bin/node_exporter',
                                         shell=True, stdin=None, executable="/bin/bash")
     set_permissions.wait()
-    os.chdir("~/")
+    os.chdir("/home/ubuntu")
     clean_up = subprocess.Popen('rm -rf node_exporter-0.18.1.linux-amd64 node_exporter-0.18.1.linux-amd64.tar.gz', shell=True, stdin=None, executable="/bin/bash")
     clean_up.wait()
     os.chdir("/etc/systemd/system/")
